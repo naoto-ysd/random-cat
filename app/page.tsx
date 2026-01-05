79c1610e0844a325e0e606d408696aa7c815d184
Yoshida <61533915+naoto-ysd@users.noreply.github.com>
2026-01-06 00:45:46 +0900

    Homeコンポーネントに非同期処理を追加し、fetchImage関数を使用して猫画像を取得するように変更

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
-export default function Home() {
+import { connection } from "next/server"
+import { fetchImage } from "./fetch-image"
+
+export default async function Home() {
+  await connection()
+
+  const image = await fetchImage()
+  console.log("Home: 画像情報を取得しました"image)
+  
   return <div>猫画像</div>
 }
