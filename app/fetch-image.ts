211b20cd2a57e93f61686f7e2622b9e252b965dc
Yoshida <61533915+naoto-ysd@users.noreply.github.com>
2026-01-06 00:12:28 +0900

    fetchImage関数を追加し、猫画像を取得するAPIを実装。Homeコンポーネントを簡素化。

---
+++ app/fetch-image.ts
@@ -0,0 +1,6 @@
+export async function fetchImage() {
+  const res = await fetch("https://api.thecatapi.com/v1/images/search");
+  const images = await res.json();
+  console.log(images);
+  return images[0]
+}
