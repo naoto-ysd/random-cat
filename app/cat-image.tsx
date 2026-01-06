bd7df2977f4f6d28e9ca67c2f9ff906a3d81fd7c
Yoshida <61533915+naoto-ysd@users.noreply.github.com>
2026-01-06 14:58:00 +0900

    CatImageコンポーネントに画像更新機能を追加し、ボタンをクリックすることで新しい猫画像を取得できるように変更

--- app/cat-image.tsx
+++ app/cat-image.tsx
@@ -1,11 +1,25 @@
+"use client"
+
+import { useState } from "react"
+import { fetchImage } from "./fetch-image"
+
 type CatImageProps = {
   url: string;
 }
 
 export function CatImage({ url }: CatImageProps){
+  const [ imageUrl, setImageUrl ] = useState(url)
+
+  const refreshImage = async () => {
+    setImageUrl("")
+    const image = await fetchImage()
+    setImageUrl(image.url)
+  }
+
   return (
     <div>
-      <img src={url} />
+      <button onClick={refreshImage}>他の猫を見る</button>
+      { imageUrl && <img src={imageUrl} /> }
     </div>
   )
 }
