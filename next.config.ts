a7d76f22dbdabaac55dbae490ee9e5fa1db8f440
Yoshida <61533915+naoto-ysd@users.noreply.github.com>
2026-01-06 15:04:17 +0900

    next.config.tsに画像設定を追加し、リモートパターンを指定

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn2.thecatapi.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
